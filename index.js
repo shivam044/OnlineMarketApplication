7d7a193586a8c18b438bf3d10ce74f470ef88a00
Shivam Ujjainwal <shivam.jobs056@gmail.com>
2024-06-17 16:05:18 -0400

    created index.js, installed mongodb, express, cors and nodemon

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to DressStore application.' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
